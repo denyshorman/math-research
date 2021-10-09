69ba7c203c8558dd432c41f19536759a1579ab30
Denys Horman <18450062+denyshorman@users.noreply.github.com>
2021-10-09 19:49:49 +0300

    Extended combination iterator.

--- src/main/kotlin/keccak/CombinationIterator.kt
+++ src/main/kotlin/keccak/CombinationIterator.kt
@@ -2,11 +2,13 @@ package keccak
 
 import keccak.util.invert
 import keccak.util.setBitsCount
+import keccak.util.toString
 import java.util.*
 
 class CombinationIterator(
     val varsCount: Int,
     val mask: BitSet = defaultMask(varsCount),
+    val algorithm: Algorithm = Algorithm.Lexicographical,
 ) {
     val combination = BitSet(varsCount)
     val solutionsCount = 1 shl mask.setBitsCount()
@@ -18,6 +20,24 @@ class CombinationIterator(
     }
 
     fun next() {
+        when (algorithm) {
+            Algorithm.Lexicographical -> lexicographicalNext()
+            Algorithm.Increasing -> increasingNext()
+        }
+
+        solutionIndex++
+    }
+
+    fun reset() {
+        solutionIndex = -1L
+        combination.clear()
+    }
+
+    override fun toString(): String {
+        return combination.toString(varsCount)
+    }
+
+    private fun lexicographicalNext() {
         var i = mask.previousSetBit(varsCount - 1)
 
         while (i >= 0) {
@@ -29,8 +49,79 @@ class CombinationIterator(
                 break
             }
         }
+    }
 
-        solutionIndex++
+    private fun increasingNext() {
+        var i = 0
+
+        while (i < varsCount) {
+            if (combination[i]) {
+                if (i == 0) {
+                    i++
+
+                    while (i < varsCount) {
+                        if (!combination[i]) {
+                            break
+                        }
+                        i++
+                    }
+
+                    i++
+
+                    while (i < varsCount) {
+                        if (combination[i]) {
+                            combination.clear(i)
+                            combination.set(i - 1)
+
+                            i--
+                            var j = i
+
+                            while (i >= 0) {
+                                if (combination[i]) {
+                                    combination.clear(i)
+                                    combination.set(j)
+                                    j--
+                                }
+
+                                i--
+                            }
+
+                            return
+                        }
+
+                        i++
+                    }
+                } else {
+                    combination.clear(i)
+                    combination.set(i - 1)
+                    return
+                }
+            }
+
+            i++
+        }
+
+        if (combination[varsCount - 1]) {
+            combination.clear()
+        } else {
+            combination.set(varsCount - 1)
+
+            i = varsCount - 2
+            var j = i
+            while (i >= 0) {
+                if (combination[i]) {
+                    combination.clear(i)
+                    combination.set(j)
+                    j--
+                }
+                i--
+            }
+        }
+    }
+
+    enum class Algorithm {
+        Lexicographical,
+        Increasing,
     }
 
     companion object {
