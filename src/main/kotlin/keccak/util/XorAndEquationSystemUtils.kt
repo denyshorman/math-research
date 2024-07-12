9e700b52cc35be563d19f28c80a9ab25884a2145
Denys Horman <18450062+denyshorman@users.noreply.github.com>
2024-07-12 23:12:56 +0300

    Add toNodeEquationSystem extension to the XorAndEquationSystem

--- src/main/kotlin/keccak/util/XorAndEquationSystemUtils.kt
+++ src/main/kotlin/keccak/util/XorAndEquationSystemUtils.kt
@@ -1,5 +1,8 @@
 package keccak.util
 
+import keccak.Bit
+import keccak.Node
+import keccak.NodeEquationSystem
 import keccak.XorAndEquationSystem
 import java.io.File
 
@@ -11,4 +14,25 @@ fun XorAndEquationSystem.toFile(
 ) {
     xorSystem.toFile(xorSystemFile, humanReadable = xorHumanReadable)
     andSystem.toFile(andSystemFile, humanReadable = andHumanReadable)
+}
+
+fun XorAndEquationSystem.toNodeEquationSystem(varPrefix: String = "x", varOffset: Int = 0): NodeEquationSystem {
+    val xorNodeSystem = xorSystem.toNodeEquationSystem(varPrefix, varOffset)
+    val andNodeSystem = andSystem.toNodeEquationSystem(varPrefix, varOffset)
+
+    val equations = Array<Node>(xorNodeSystem.rows + andNodeSystem.rows) { Bit() }
+
+    var i = 0
+    while (i < xorNodeSystem.rows) {
+        equations[i] = xorNodeSystem.equations[i]
+        i++
+    }
+
+    var j = 0
+    while (j < andNodeSystem.rows) {
+        equations[i + j] = andNodeSystem.equations[j]
+        j++
+    }
+
+    return NodeEquationSystem(equations)
 }
